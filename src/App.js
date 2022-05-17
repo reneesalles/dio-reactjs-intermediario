9eefdd008cc8f4d05e1ec09647d51e5bc5994e84
reneesalles <reneesalles@hotmail.com>
2022-05-17 21:15:17 +0000

    adicionando navegação para melhorar o visual

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import { useState } from 'react';
 import './App.css';
 import EstadosContainer from './estados/EstadosContainer';
 import EstilizacaoContainer from './estilizacao/EstilizacaoContainer';
 
 function App() {
+  const paginas = {
+    estilizacao: 'estilizacao',
+    estados: 'estados',
+    formularios: 'formularios'
+  }
+
+  const paginasArr = () => {
+    let arr = [];
+    for (let pagina in paginas) {
+      arr.push(pagina);
+    }
+    return arr;
+  }
+
+  const [state, setState] = useState({
+    pagina: paginas.estilizacao
+  });
+
+  const mudarPagina = (pagina) => {
+    setState({ pagina: pagina });
+  };
+
+  const renderListaPaginas = () => {
+    return paginasArr().map(pagina => <li>{renderBtnPagina(pagina)}</li>);
+  };
+
+  const renderBtnPagina = (pagina) => {
+    return <button className={state.pagina === pagina ? 'active' : null} onClick={() => mudarPagina(pagina)}>{pagina}</button>
+  };
+
   return (
     <div>
-      <EstilizacaoContainer />
-      <EstadosContainer />
+      <nav>
+        <ul>
+          {renderListaPaginas()}
+        </ul>
+      </nav>
+      <main>
+        {state.pagina === paginas.estilizacao && <EstilizacaoContainer />}
+        {state.pagina === paginas.estados && <EstadosContainer />}
+      </main>
     </div>
   );
 }
--- src/estados/EstadosContainer.jsx
+++ src/estados/EstadosContainer.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HooksComponent from './HooksComponent';
 import StatefullComponent from './StatefullComponent';
 import StatelessComponent from './StatelessComponent';
 
 const EstadosContainer = () => {
+    const estados = {
+        statefull: 'statefull',
+        stateless: 'stateless',
+        hooks: 'hooks'
+    }
+
+    const estadosArr = () => {
+        let arr = [];
+        for (let estado in estados) {
+            arr.push(estado);
+        }
+        return arr;
+    }
+
+    const [state, setState] = useState({
+        estado: estados.statefull
+    });
+
+    const mudarEstado = (estado) => {
+        setState({ estado: estado });
+    };
+
+    const renderListaEstados = () => {
+        return estadosArr().map(estado => <li>{renderBtnEstado(estado)}</li>);
+    };
+
+    const renderBtnEstado = (estado) => {
+        return <button className={state.estado === estado ? 'active' : null} onClick={() => mudarEstado(estado)}>{estado}</button>
+    };
+
     return (
         <div>
             <h3>Estados</h3>
-            <StatefullComponent />
-            <StatelessComponent items={[
-                'Tomate',
-                'Alface',
-                'Melancia'
-            ]} />
-            <HooksComponent />
+            <nav>
+                <ul>
+                    {renderListaEstados()}
+                </ul>
+            </nav>
+            <main>
+                {state.estado === estados.statefull && <StatefullComponent />}
+                {state.estado === estados.stateless && <StatelessComponent items={['Tomate', 'Alface', 'Melancia']} />}
+                {state.estado === estados.hooks && <HooksComponent />}
+            </main>
         </div>
     )
 };
